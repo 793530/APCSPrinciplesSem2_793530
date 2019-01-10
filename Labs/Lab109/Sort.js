a16b5dfcd3980297df8126f522ad4403fd666e20
Kalpa Yakupitiyage <42980790+793530@users.noreply.github.com>
2019-01-10 07:50:23 -0800

    Commit

---
+++ Labs/Lab109/Sort.js
@@ -0,0 +1,53 @@
+/*
+Kalpa Yakupitiyage
+01/10/2019
+Sort
+*/
+
+var txt;
+
+function preload(){           //This opens the text file
+  txt = loadStrings("words.txt");
+
+}
+
+function setup() {
+
+  console.log(txt)            //
+  Sort(txt);            //
+
+
+}
+
+function Sort(txt){               //The code keeps repeating until it is false
+
+  var length = txt.length
+  for(
+
+        }
+
+
+
+
+
+      }
+
+
+
+  }
+
+
+}
+
+/*
+for(var i + length-1; i > 0; i--){
+    for(var j = 0; j > i; j++ ){
+      if(txt[j] > txt[j+1]){
+        var tmp =txt[j];
+        txt[j] = txt[j+1];
+        txt[j+1 = tmp];
+
+Using bubbleSort as a guide to help make a this sort
+
+
+*/
