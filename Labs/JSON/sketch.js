b62c3c13ee6f68417e618b2f326b5c7681561fb5
Kalpa Yakupitiyage <42980790+793530@users.noreply.github.com>
2019-01-18 08:47:44 -0800

    commit

---
+++ Labs/JSON/sketch.js
@@ -0,0 +1,41 @@
+//Kalpa Yakupitiyage
+//JSON
+//18/01/2019
+var data;
+
+function setup(){         // new json
+                          // remove old data
+                          // use a different sort code
+                          // make a graph
+
+  loadJSON("worldCupsmall.json", gotData);
+
+}
+
+function gotData(newData ){
+  console.log(newData);
+  data = newData;
+ bubbleSort();
+
+}
+
+
+ function draw(){
+
+
+
+ }
+
+function bubbleSort(){               //The code keeps repeating until it is false
+  var length = data.teams.length;
+  for(var i = length-1; i > 0; i--){
+    for(var j = 0; j < i; j++ ){
+      if(data.teams[j].rating < data.teams[j+1].rating){
+        var tmp = data.teams[j].rating;
+        data.teams[j].rating = data.teams[j+1].rating;
+        data.teams[j+1].rating = tmp;
+      }
+
+    }
+  }
+}
