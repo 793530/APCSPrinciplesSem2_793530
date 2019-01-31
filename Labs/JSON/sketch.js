00def6ba17a53c24f9ed2547c3226aa8bcec2533
Kalpa Yakupitiyage <42980790+793530@users.noreply.github.com>
2019-01-31 14:53:07 -0800

    commit

--- Labs/JSON/sketch.js
+++ Labs/JSON/sketch.js
@@ -1,42 +1,38 @@
 //Kalpa Yakupitiyage
 //JSON
-//18/01/2019
+//31/01/2019
 var data;
 
-function preloader(){
-  data = loadJSON("data.json");
-
-}
+// function preloader(){
+//   data = loadJSON("data.json");
+// }
 
 function setup(){         // new json
-                          // remove old data
-                          // use a different sort code
-                          // make a graph
-
-  loadJSON("data.json", gotData);
-
+  // make a graph
+   loadJSON("data.json", gotData);
+//   var cnv = createCanvas(500, 500);
+   background(50,120,66);
 }
 
 function gotData(newData ){
   console.log(newData);
   data = newData;
- selectSort();
-
+  selectSort();
+  console.log(newData);
 }
 
 
  function draw(){
-
  }
 
 function selectSort(){               //The code keeps repeating until it is false
 
-  for(var i = data.Murder.length-1; i > 0; i--){
+  for(var i = data.crime.length-1; i > 0; i--){
     for(var j = 0; j < i; j++ ){
-      if(data.Murder[j].rating < data.Murder[j+1].rating){
-        var tmp = data.Murder[j].rating;
-        data.Murder[j].rating = data.Murder[j+1].rating;
-        data.Murder[j+1].rating = tmp;
+      if(data.crime[j].Murder < data.crime[j+1].Murder){
+        var tmp = data.crime[j].Murder;
+        data.crime[j].Murder = data.crime[j+1].Murder;
+        data.crime[j+1].Murder = tmp;
       }
 
     }
