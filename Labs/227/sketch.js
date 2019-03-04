1a50690b9f866ad8df4da965924229059f13bf49
Knight <42980790+793530@users.noreply.github.com>
2019-03-04 08:40:58 -0800

    Commit

---
+++ Labs/227/sketch.js
@@ -0,0 +1,33 @@
+//  Global variables
+var b1;
+var balls = [];
+// put setup code here
+function setup() {
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(20,20,20);
+  var loc = createVector(random(width), random(height));
+  var rad = 25;
+  var col = color(0,255,0);
+  b1 = new Ball(loc,rad,col);
+  loadBalls(500);
+}
+
+function draw() {
+  background(20,20,20,40);
+
+  b1.run();
+
+  for(var i = 0; i<balls.length; i++){
+     balls[i].run();
+}
+}
+
+function loadBalls(numBalls){
+    for(var i = 0; i<numBalls; i++){
+      var loc = createVector(random(width),(height/2));
+      var rad = 15;
+      var col = color(255,0,0);
+      balls.push(new Ball(loc,rad,col))
+    }
+}
