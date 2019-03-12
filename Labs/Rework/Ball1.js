330f36a59f3e98946a97599325ceb62581586fe6
Knight <42980790+793530@users.noreply.github.com>
2019-03-12 12:20:15 -0700

    commit

--- Labs/Rework/Ball1.js
+++ Labs/Rework/Ball1.js
@@ -9,21 +9,16 @@ function Orbiter(parenLoc, id){
   this.dtp = 150;
   this.clr = color(random(225),random(225),random(225),random(225));
 
-
   this.update = function(){
     this.loc.x = parentLoc.x + cos(this.angle)*this.dtp;
     this.loc.y = parentLoc.y + sin(this.angle)*this.dtp;
     this.angle += 0.01;
-
-
 
   }
 
   this.render = function(){
     fill(this.clr);
     ellipse(this.loc.x, this.loc.y ,5 ,5)
-    
-
 
   }
 }
