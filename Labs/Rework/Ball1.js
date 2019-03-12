d7e2e6fb2044767a42c168e9d7359874f00d8609
Knight <42980790+793530@users.noreply.github.com>
2019-03-12 12:18:31 -0700

    commit

--- Labs/Rework/Ball1.js
+++ Labs/Rework/Ball1.js
@@ -1,5 +1,29 @@
 
-/*git test
+/*
 **  Kalpa Yakupitiyage
 */
-function
+function Orbiter(parenLoc, id){
+
+  this.loc = createVector(parentLoc.x, parentLoc.y);
+  this.angle = random(TWO_P1);
+  this.dtp = 150;
+  this.clr = color(random(225),random(225),random(225),random(225));
+
+
+  this.update = function(){
+    this.loc.x = parentLoc.x + cos(this.angle)*this.dtp;
+    this.loc.y = parentLoc.y + sin(this.angle)*this.dtp;
+    this.angle += 0.01;
+
+
+
+  }
+
+  this.render = function(){
+    fill(this.clr);
+    ellipse(this.loc.x, this.loc.y ,5 ,5)
+    
+
+
+  }
+}
